9e072079dca2f71b2aaa3076bc6b741579e3ad2a
Sumit <grt.sumitr25@gmail.com>
2020-01-09 10:25:06 +0530

    fixes

--- frontend/src/containers/Dashboard/saga.js
+++ frontend/src/containers/Dashboard/saga.js
@@ -2,10 +2,9 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { getRequest } from '../../utils/api';
 import { getConfig, successInGettingConfig, errorInGettingConfig } from './reducer';
 
-// worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* fetchConfig(action) {
    try {
-      const data = yield call(getRequest, 'http://localhost:3001/data1.json');
+      const data = yield call(getRequest, 'http://localhost:3001/data.json');
       yield put({ type: successInGettingConfig.type, payload: data });
    } catch (e) {
       yield put({ type: errorInGettingConfig.type, payload: e.message });
